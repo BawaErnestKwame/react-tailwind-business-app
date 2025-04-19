e46f02193732366551fc91fbf1527662ec025fe4
Bawa Ernest Kwame <bawaernest926@gmail.com>
2025-04-19 22:56:31 +0000

    responsive nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -12,12 +14,18 @@ const Navbar = () => {
   ];
 
   return (
-    <div className='fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm z-50 border-b border-gray-100 shadow-sm'>
-      <div className="w-full container max-auto flex items-center justify-between px-4 sm:px-6 lg:px-8 md:h-20 h-16">
-        <div className="flex items-center gap-1 cursor-pointer">
+    <div className= 'fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm z-50 border-b border-gray-100 shadow-sm'>
+      <div className= "w-full container max-auto flex items-center justify-between px-4 sm:px-6 lg:px-8 md:h-20 h-16">
+        <div className= "flex items-center gap-1 cursor-pointer">
           <div className="w-4 h-4 bg-blue-600 rounded-full opacity-75 hover:opacity-100 transition-opacity"></div>
+
           <div className="w-4 h-4 bg-red-600 rounded-full opacity-100 hover:opacity-75 transition-opacity -ml-2"></div>
+
         </div>
+
+        <button className= 'md:hidden p-2' onClick={()=>setIsMenuOpen(!isMenuOpen)}>
+            {isMenuOpen ? <CloseIcon  className='size-4'/> :<MenuIcon className='size-4'/>}
+          </button>
 
         <div className="hidden md:flex items-center gap-10">
           {naveLinks.map((link, index) => (
@@ -40,6 +48,36 @@ const Navbar = () => {
             <a href='#newsletter'>Get In Torch</a>
         </button>
       </div>
+
+      {
+        isMenuOpen && (
+          <div className="md:hidden bg-white border-t border-gray-100 py-4">
+          
+          <div className="container mx-auto px-4 space-y-3">
+
+          {naveLinks.map((link, index)=>
+          <a
+          key={index}
+          onClick={()=>{
+            setActiveLink(link.href);
+            setIsMenuOpen(false)
+          }}
+          href={link.href} className= {`block text-sm font-medium py-2 hover:text-blue-700 ${naveLinks=== link.href ? "text-blue-600t" : "text-gray-900"}`}>{link.label}</a>
+
+          
+          )}
+
+  <button className= 'w-full hover:bg-blue-800 transition-all bg-blue-700 py-2 px-4 text-white rounded-lg hover:shadow-blue-900 '> 
+            <a href='#newsletter'>Get In Torch</a>
+        </button>
+          
+          </div>
+          
+          </div>
+        )
+      }
+
+       
     </div>
   );
 };
